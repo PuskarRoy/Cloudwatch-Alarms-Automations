42580279ef0b7272ba8da59d01c7edcc621554c1
Puskar Roy <roypuskar2003@gmail.com>
2026-01-14 15:52:02 +0530

    Create Generate_Inventory.sh

---
+++ assets/Generate_Inventory.sh
@@ -0,0 +1,36 @@
+set -e
+
+OUT_FILE="./assets/inventory.aws_ec2.yml"
+
+# extract the region value (supports quotes)
+REGION=$REGION_NAME
+
+if [ -z "$REGION" ]; then
+  echo "Could not parse region from Environment." >&2
+  exit 3
+fi
+
+BUCKET_NAME=$BUCKET_NAME
+
+if [ -z "$BUCKET_NAME" ]; then
+  echo "Could not parse bucket_name from Environment. " >&2
+  exit 4
+fi
+
+cat > "$OUT_FILE" <<EOF
+---
+plugin: amazon.aws.aws_ec2
+
+regions: $REGION
+
+filters:
+  "tag:Alarms-Automation": "Yes"
+  instance-state-name: running
+
+hostnames: instance-id
+
+compose:
+  ansible_connection: '"aws_ssm"'
+  ansible_aws_ssm_bucket_name: '"$BUCKET_NAME"'
+  ansible_aws_ssm_region: '"$REGION"'
+EOF
