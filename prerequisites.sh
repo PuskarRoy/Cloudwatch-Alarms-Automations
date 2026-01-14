f24e2e923e2c5db28e187a46a2a24ba084d19c2a
Puskar Roy <roypuskar2003@gmail.com>
2026-01-14 15:28:46 +0530

    Update prerequisites.sh

--- prerequisites.sh
+++ prerequisites.sh
@@ -14,46 +14,8 @@ sudo dpkg -i session-manager-plugin.deb
 
 sudo rm -fr amazon-ssm-agent.deb session-manager-plugin.deb
 
-sudo snap install aws-cli --classic
+sudo bash ./assets/Aws_Cli_Install.sh
 
-set -e
-
-OUT_FILE="./assets/inventory.aws_ec2.yml"
-
-# extract the region value (supports quotes)
-REGION=$REGION_NAME
-
-if [ -z "$REGION" ]; then
-  echo "Could not parse region from Environment." >&2
-  exit 3
-fi
-
-BUCKET_NAME=$BUCKET_NAME
-
-if [ -z "$BUCKET_NAME" ]; then
-  echo "Could not parse bucket_name from Environment. " >&2
-  exit 4
-fi
-
-cat > "$OUT_FILE" <<EOF
----
-plugin: amazon.aws.aws_ec2
-
-regions: $REGION
-
-filters:
-  "tag:Alarms-Automation": "Yes"
-  instance-state-name: running
-
-hostnames: instance-id
-
-compose:
-  ansible_connection: '"aws_ssm"'
-  ansible_aws_ssm_bucket_name: '"$BUCKET_NAME"'
-  ansible_aws_ssm_region: '"$REGION"'
-EOF
-
-echo "Generated $OUT_FILE"
 echo "******************************************************"
-echo "   $REGION_NAME $BUCKET_NAME                USE TAG : Alarms-Automation: Yes"
+echo "                USE TAG : Alarms-Automation: Yes"
 echo "******************************************************"
